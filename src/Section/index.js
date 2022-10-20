5bb15a22a9f525550f73440266d46d57edf48d68
Artur Gajewski <arturgaj6@wp.pl>
2022-10-20 20:27:32 +0200

    delete unnecessary div and remove <p> in document

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,15 +1,14 @@
 import "./style.css";
 
-const Section = ({title,description,body}) => (
+const Section = ({ title, description, information, body }) => (
   <section>
-  <div className="section__title">{title}</div>
-  <p className="section__description">
-    {description}
-  </p>
-  <p className="section__description">
-   {body}
-  </p>
-</section>
+    <div className="section__title">{title}</div>
+    <div className="section__description">
+      {description}
+      {body}
+      {information}
+    </div>
+  </section>
 );
 
 export default Section;
