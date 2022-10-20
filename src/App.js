76af4c14285cb89144273beed9bc8321542f30a2
Artur Gajewski <arturgaj6@wp.pl>
2022-10-20 18:48:39 +0200

    divided into components/added [ currencies] to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import { useState } from "react";
+import Container from "./Container";
 import Form from "./Form";
+import Header from "./Header";
+import Section from "./Section";
+import Buttons from "./Button";
+
+const currencies = [
+  {
+    id: 1,
+    name: "EURO",
+    value: 4.89,
+  },
+  {
+    id: 2,
+    name: "DOLAR",
+    value: 4.93,
+  },
+  {
+    id: 3,
+    name: "GBP",
+    value: 5.44,
+  },
+  {
+    id: 4,
+    name: "CZK",
+    value: 0.21,
+  },
+];
 
 function App() {
   return (
-   <Form />
+   <Container>
+    <Header title="Kantor Walut" />
+<Section 
+title="Wybierz walutę"
+description="Pola oznaczone * są
+wymagane."
+body={<Form 
+button={<Buttons />}
+/>}
+/>
+    </Container>
   );
 }
 
