60cda40250e2e313c5af5159130266f01b3c0fea
Artur Gajewski <arturgaj6@wp.pl>
2022-10-21 18:20:31 +0200

    transfer [currencies] to component currencies

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
-import { useState } from "react";
 import Container from "./Container";
 import Form from "./Form";
 import Header from "./Header";
 import Section from "./Section";
 import Buttons from "./Button";
-
-const currencies = [
-  {
-    id: 1,
-    name: "EURO",
-    value: 4.84,
-  },
-  {
-    id: 2,
-    name: "DOLAR",
-    value: 4.95,
-  },
-  {
-    id: 3,
-    name: "GBP",
-    value: 5.57,
-  },
-  {
-    id: 4,
-    name: "CZK",
-    value: 0.19,
-  },
-];
+import Result from "./Result";
 
 function App() {
-const [currency,setCurrency] = useState();
-
-const onChangeSetCurrency = () => {
-  console.log("Witaj w konsoli programisty :)");
-};
-console.log(onChangeSetCurrency());
-
   return (
-   <Container>
-     <Header title="Kantor Walut" />
-      <Section 
-    title="Wybierz walutę *"
-    description="Pola oznaczone * są wymagane."
-    body={<Form 
-    button={<Buttons />} 
-      />}
-    information="Kursy walut na dzień 20.10.2022 pobrane z NBP"
-     />
-    
+    <Container>
+      <Header title="Kantor Walut" />
+      <Section
+        title="Wybierz walutę *"
+        description="Pola oznaczone * są wymagane."
+        body={<Form 
+          result={<Result />}
+          button={<Buttons />} />}
+        information="Kursy walut na dzień 20.10.2022 pobrane z NBP"
+      />
     </Container>
   );
 }
