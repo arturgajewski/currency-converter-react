d1aa03720e266c5c6139f2a1353315f11895cd63
Artur Gajewski <arturgaj6@wp.pl>
2022-11-03 14:05:47 +0100

    currencies move to new folder

---
+++ src/currencies/currencies/index.js
@@ -0,0 +1,32 @@
+export const currencies = [
+  {
+    fullName: "Euro",
+    name: "Euro",
+    id: 0,
+    rate: 4.84,
+  },
+  {
+    fullName: "Dolar Amerykański",
+    name: "Dolar",
+    id: 1,
+    rate: 4.95,
+  },
+  {
+    fullName: "Funt Brytyjski",
+    name: "GBP",
+    id: 2,
+    rate: 5.57,
+  },
+  {
+    fullName: "Frank Szwajcarski",
+    name: "CHF",
+    id: 3,
+    rate: 4.93,
+  },
+  {
+    fullName: "Czeska Korona",
+    name: "CZK",
+    id: 4,
+    rate: 0.19,
+  },
+];
